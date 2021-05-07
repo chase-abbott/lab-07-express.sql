4e27865aadcfcd67b61bad840d74857ec839ed31
Chase <cabbott93@gmail.com>
2021-05-07 12:45:21 -0700

    add get user route

--- data/users.js
+++ data/users.js
@@ -3,5 +3,10 @@ export const users = [
     name: 'Chase',
     email: 'cabbott93@gmail.com',
     password: 'x'
+  },
+  {
+    name: 'Chase2',
+    email: 'cabbott93@gmail.com',
+    password: 'y'
   }
 ];
